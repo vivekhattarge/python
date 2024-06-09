e6a468ba7eccb3c27d16acf5e19f7a7268401ebd
vivekhattarge <vivek.hattarge@gmail.com>
2024-06-09 18:41:43 +0530

    What : Python program for decorators

---
+++ 07_Decorators/02_Solution.py
@@ -0,0 +1,14 @@
+
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        args_value = ', '.join(str(arg) for arg in args)
+        kwargs_value = ', '.join(f"{k}={v}" for k, v in kwargs.items())
+        print(f"calling: {func.__name__} with args {args_value} and kwargs {kwargs_value}")
+        return func(*args, **kwargs)
+    return wrapper
+
+@decorator
+def greet(name, greeting="Hello"):
+    print(f"{greeting}, {name}")
+
+greet("Chai", greeting="Hello ")
